998816b1e004308644b8b0b137412fb84809406e
rbrat <r.bratchenko@yandex.ru>
2025-05-06 22:55:38 +0500

    refactored roll functions

--- src/d6_roller/engine/core.py
+++ src/d6_roller/engine/core.py
@@ -8,19 +8,26 @@ from ..schemas.engine import Sequence
 from ..schemas.lazyint import RandInt
 from ..schemas.dice import d6
 
+CHECK_DICT = {
+    True: np.ndarray.__ge__,
+    False: np.ndarray.__lt__,
+}
 
-def roll_hit(dice: int, threshold: int | None) -> int:
+
+def make_rolls(dice: int, threshold: int | None, is_positive: bool = False) -> int:
     if not threshold:
         return dice
     roll = np.array([random.choice(d6) for _ in np.arange(dice)])
-    return roll[roll >= threshold].size
+    logger.info("Dice rolled %s", roll)
+    return roll[CHECK_DICT[is_positive](roll, threshold)].size
 
 
-def roll_save(dice: int, threshold: int | None) -> int:
-    if not threshold:
-        return dice
-    roll = np.array([random.choice(d6) for _ in np.arange(dice)])
-    return roll[roll < threshold].size
+def roll_positive_check(dice: int, threshold: int | None) -> int:
+    return make_rolls(dice, threshold, True)
+
+
+def roll_negative_check(dice: int, threshold: int | None) -> int:
+    return make_rolls(dice, threshold, False)
 
 
 def inflict_damage(dice: int, value: RandInt) -> int:
@@ -28,12 +35,12 @@ def inflict_damage(dice: int, value: RandInt) -> int:
     return np.sum(roll)
 
 
-def make_rolls(sequence: Sequence) -> int:
-    dice = roll_hit(sequence.attacks.get, sequence.threshold.to_hit)
+def run_sequence(sequence: Sequence) -> int:
+    dice = roll_positive_check(sequence.attacks.get, sequence.threshold.to_hit)
     logger.info('rolled %s successful hits', dice)
-    dice = roll_hit(dice, sequence.threshold.to_wound)
+    dice = roll_positive_check(dice, sequence.threshold.to_wound)
     logger.info('rolled %s successful wounds', dice)
-    dice = roll_save(dice, sequence.threshold.save)
+    dice = roll_negative_check(dice, sequence.threshold.save)
     logger.info('rolled %s failed saves', dice)
     return inflict_damage(dice, sequence.damage)
 
@@ -46,5 +53,5 @@ def simulate(attacker: Weapon, defender: Profile) -> int:
     )
     logger.info('Attacking %s with %s', defender, attacker)
     logger.info(sequence)
-    damage = make_rolls(sequence)
+    damage = run_sequence(sequence)
     return damage
